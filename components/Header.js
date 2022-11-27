b27483a651658bea4c43ac5b25bfd5db48de96e2
Heriel143 <herieldavid143@gmail.com>
2022-11-28 01:43:10 +0300

    update the import for Image of next

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React from "react";
 import {
   MagnifyingGlassIcon,
@@ -22,64 +22,64 @@ const Header = () => {
   // console.log(session);
 
   return (
-    <div className=" shadow-sm boreder-b bg-white sticky top-0 z-50">
-      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+    <div className=' shadow-sm boreder-b bg-white sticky top-0 z-50'>
+      <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/** Left */}
         <div
           onClick={() => router.push("/")}
-          className="relative hidden lg:inline-grid w-24 cursor-pointer"
+          className='relative hidden lg:inline-grid w-24 cursor-pointer'
         >
           <Image
-            src="https://links.papareact.com/ocw"
-            layout="fill"
-            objectFit="contain"
+            src='https://links.papareact.com/ocw'
+            layout='fill'
+            objectFit='contain'
           />
         </div>
         <div
           onClick={() => router.push("/")}
-          className="relative lg:hidden  w-10 flex-shrink-0 cursor-pointer "
+          className='relative lg:hidden  w-10 flex-shrink-0 cursor-pointer '
         >
           <Image
-            src="https://links.papareact.com/jjm"
-            layout="fill"
-            objectFit="contain"
+            src='https://links.papareact.com/jjm'
+            layout='fill'
+            objectFit='contain'
           />
         </div>
         {/** Middle: input field */}
-        <div className="relative mt-1 p-3 rounded-md sm:max-w-sm">
-          <div className=" absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+        <div className='relative mt-1 p-3 rounded-md sm:max-w-sm'>
+          <div className=' absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500' />
           </div>
           <input
-            type="text"
-            className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
-            placeholder="Search"
+            type='text'
+            className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
+            placeholder='Search'
           />
         </div>
         {/** Right */}
-        <div className="flex items-center justify-end space-x-4">
-          <HomeIcon onClick={() => router.push("/")} className="navBtn" />
-          <Bars3Icon className="h-6 md:hidden cursor-pointer" />
+        <div className='flex items-center justify-end space-x-4'>
+          <HomeIcon onClick={() => router.push("/")} className='navBtn' />
+          <Bars3Icon className='h-6 md:hidden cursor-pointer' />
 
           {session ? (
             <>
-              <div className=" relative navBtn">
-                <PaperAirplaneIcon className=" -rotate-45" />
-                <div className=" absolute -top-1 -right-2 text-xs h-4 w-4 bg-red-400 rounded-full flex items-center justify-center animate-pulse text-white">
+              <div className=' relative navBtn'>
+                <PaperAirplaneIcon className=' -rotate-45' />
+                <div className=' absolute -top-1 -right-2 text-xs h-4 w-4 bg-red-400 rounded-full flex items-center justify-center animate-pulse text-white'>
                   3
                 </div>
               </div>
               <PlusCircleIcon
                 onClick={() => setOpen(true)}
-                className="navBtn"
+                className='navBtn'
               />
-              <UserGroupIcon className="navBtn" />
-              <HeartIcon className="navBtn" />
+              <UserGroupIcon className='navBtn' />
+              <HeartIcon className='navBtn' />
               <img
                 onClick={signOut}
                 src={session?.user?.image}
-                alt="profile pic"
-                className="h-10 w-10 rounded-full cursor-pointer"
+                alt='profile pic'
+                className='h-10 w-10 rounded-full cursor-pointer'
               />
             </>
           ) : (
